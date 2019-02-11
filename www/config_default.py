435fbedcea235ba3674b5e053f7a3f9276f830ac
JackSparrow52 <jack_sparrow_52@163.com>
2019-02-11 14:51:41 +0800

    config file complete

---
+++ www/config_default.py
@@ -0,0 +1,13 @@
+configs = {
+        'debug':True,
+        'db':{
+                'host': '172.16.60.236',
+                'port': 3306,
+                'user': 'www-data',
+                'password': 'www-data',
+                'db': 'awesome'
+        },
+        'session':{
+                'secret': 'Awesome'
+        }
+}
