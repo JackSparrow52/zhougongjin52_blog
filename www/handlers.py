882be6a9093569960f7d13d287356dc7f3acea82
JackSparrow52 <jack_sparrow_52@163.com>
2019-02-18 02:47:16 +0800

    register and signin test success

--- www/handlers.py
+++ www/handlers.py
@@ -6,8 +6,10 @@ import hashlib
 import base64
 import asyncio
 
+from aiohttp import web
+
 from coroweb import get, post
-from apis import APIValueError, APIResourceNotFoundError
+from apis import APIValueError, APIResourceNotFoundError, APIError
 
 from models import User, Comment, Blog, next_id
 from config import configs
@@ -70,6 +72,35 @@ async def api_register_user(*, email, name, passwd):
     r = web.Response()
     r.set_cookie(COOKIE_NAME, user2cookie(user, 86400), max_age=86400, httponly=True)
     user.password = '******'
+    r.content_type = 'application/json'
+    r.body = json.dumps(user, ensure_ascii=False).encode('utf-8')
+    return r
+
+@get('/signin')
+def signin():
+    return {
+            '__template__': 'signin.html'
+    }
+    
+@post('/api/authenticate')
+async def authenticate(*, email, passwd):
+    if not email:
+        raise APIValueError('email', 'Invalid email.')
+    if not passwd:
+        raise APIValueError('passwd', 'Invalid password.')
+    users = await User.findAll('email=?', [email])
+    if len(users) == 0:
+        raise APIValueError('email', 'Email not exist.')
+    user = users[0]
+    sha1 = hashlib.sha1()
+    sha1.update(user.id.encode('utf-8'))
+    sha1.update(b':')
+    sha1.update(passwd.encode('utf-8'))
+    if user.passwd != sha1.hexdigest():
+        raise APIValueError('passwd', 'Invalid passwd.')
+    r = web.Response()
+    r.set_cookie(COOKIE_NAME, user2cookie(user, 86400), max_age=86400, httponly=True)
+    user.passwd = "******"
     r.content_type = 'application/json'
     r.body = json.dumps(user, ensure_ascii=False).encode('utf-8')
     return r
